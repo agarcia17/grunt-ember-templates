cb71eb3a7fd9cfd56a0a4912b2559ce0d69d8c16
Armando Garcia <armando.garcia@evhc.net>
2018-05-24 11:49:47 -0700

    Remove logging per file

--- tasks/ember-templates.js
+++ tasks/ember-templates.js
@@ -6,12 +6,12 @@
  * https://github.com/dgeb/grunt-ember-templates/blob/master/LICENSE
  */
 
-function manualCompile(templateCompilerPath, template, grunt){
+function manualCompile(templateCompilerPath, template, grunt) {
   'use strict';
 
   var fs = require('fs'),
-      vm = require('vm'),
-      templateCompilerJs;
+    vm = require('vm'),
+    templateCompilerJs;
 
   try {
     templateCompilerJs = fs.readFileSync(templateCompilerPath, 'utf8');
@@ -19,7 +19,9 @@ function manualCompile(templateCompilerPath, template, grunt){
     if (e.code !== 'ENOENT') {
       throw e;
     }
-    grunt.fail.warn('ember-template-compiler.js is not found. You can install with `bower install ember --save`. If you would like to use a custom location you can setup `templateCompilerPath` option in Gruntfile.js. It has to point to ember-template-compiler.js which bundled with Ember.js. The default path: bower_components/ember/ember-template-compiler.js');
+    grunt.fail.warn(
+      'ember-template-compiler.js is not found. You can install with `bower install ember --save`. If you would like to use a custom location you can setup `templateCompilerPath` option in Gruntfile.js. It has to point to ember-template-compiler.js which bundled with Ember.js. The default path: bower_components/ember/ember-template-compiler.js'
+    );
   }
 
   // Create a context into which we will load both the ember template compiler
@@ -39,7 +41,10 @@ function manualCompile(templateCompilerPath, template, grunt){
   vm.runInContext(templateCompilerJs, context, 'ember-template-compiler.js');
 
   // Compile the template
-  vm.runInContext('compiledJS = ((typeof Ember !== "undefined" && Ember.HTMLBars) || module.exports || exports).precompile(template);', context);
+  vm.runInContext(
+    'compiledJS = ((typeof Ember !== "undefined" && Ember.HTMLBars) || module.exports || exports).precompile(template);',
+    context
+  );
 
   return context.compiledJS;
 }
@@ -56,7 +61,7 @@ module.exports = function(grunt) {
 
     if (contents.length > 0) {
       grunt.file.write(dest, contents.join('\n\n'));
-      grunt.log.writeln('File "' + dest + '" created.');
+      // grunt.log.writeln('File "' + dest + '" created.');
     }
   };
 
@@ -66,7 +71,9 @@ module.exports = function(grunt) {
       amd: false,
       concatenate: true,
       precompile: true,
-      templateName: function(name) { return name; },
+      templateName: function(name) {
+        return name;
+      },
       templateNameFromFile: function(file) {
         // `templateBaseDir` is an alias for `templateBasePath`
         var templateBasePath = options.templateBasePath || options.templateBaseDir;
@@ -96,10 +103,10 @@ module.exports = function(grunt) {
     // Iterate files
     this.files.forEach(function(f) {
       var processedTemplates = [],
-          output = [],
-          name,
-          customAmd,
-          contents;
+        output = [],
+        name,
+        customAmd,
+        contents;
 
       if (options.amd) {
         customAmd = typeof options.amd === 'string' && options.amd !== 'true';
@@ -109,11 +116,9 @@ module.exports = function(grunt) {
 
       f.src.forEach(function(file) {
         try {
-
           name = options.templateNameFromFile(file);
 
           if (options.precompile) {
-
             var template = grunt.file.read(file);
             var compiledTemplate;
 
@@ -128,15 +133,19 @@ module.exports = function(grunt) {
             contents = 'Ember.' + options.templateNamespace + '.template(' + compiledTemplate + ')';
           } else {
             // Wrap raw (uncompiled) template
-            contents = 'Ember.' + options.templateNamespace + '.compile(' + JSON.stringify(grunt.file.read(file)) + ')';
+            contents =
+              'Ember.' +
+              options.templateNamespace +
+              '.compile(' +
+              JSON.stringify(grunt.file.read(file)) +
+              ')';
           }
 
           processedTemplates.push({
             name: name,
             contents: options.templateRegistration(name, contents)
           });
-
-        } catch(e) {
+        } catch (e) {
           grunt.log.error(e.message);
           grunt.log.error(e.stack);
           grunt.fail.warn('Ember Handlebars failed to compile ' + file + '.');
@@ -148,21 +157,28 @@ module.exports = function(grunt) {
         output = output.concat(
           processedTemplates.map(function(template) {
             return template.contents;
-          }));
+          })
+        );
 
         writeFile(output, f.dest, options);
-
       } else {
         // Write a file for each template
         processedTemplates.forEach(function(template) {
-          writeFile(output.concat(template.contents),
-                    path.join(f.dest, template.name + '.js'),
-                    options);
+          writeFile(
+            output.concat(template.contents),
+            path.join(f.dest, template.name + '.js'),
+            options
+          );
         });
       }
-
     });
+
+    grunt.log.writeln('Created "' + this.files.length + '" files.');
   };
 
-  grunt.registerMultiTask('emberTemplates', 'Compile Handlebars templates for Ember.', emberTemplatesTask);
+  grunt.registerMultiTask(
+    'emberTemplates',
+    'Compile Handlebars templates for Ember.',
+    emberTemplatesTask
+  );
 };
